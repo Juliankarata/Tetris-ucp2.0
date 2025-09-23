8e1ede421e9dd9fbad7cd119bebe937b8b2299b8
Francisco Raúl Sosa Wilka <looquesea543@gmail.com>
2025-09-22 22:01:58 -0300

    Adicion_colision

--- demo/src/test/java/com/tetris/CaidaLibreTest.java
+++ demo/src/test/java/com/tetris/CaidaLibreTest.java
@@ -26,6 +26,33 @@ public class CaidaLibreTest {
     }
 
     @Test
+    public void testColisionEntrePiezas() {
+        Juego juego = new Juego(4, 6);
+        Tablero tablero = juego.getTablero();
+
+        // Colocar la primera pieza y dejarla caer hasta el fondo
+        Pieza piezaO = new PiezaO();
+        tablero.ponerPiezaActual(piezaO);
+        while (tablero.obtenerPiezaActual() != null) {
+            tablero.moverAbajo();
+        }
+
+        // Colocar una segunda pieza encima y moverla hacia abajo hasta que colisione
+        Pieza piezaI = new PiezaI();
+        tablero.ponerPiezaActual(piezaI);
+        boolean colisiono = false;
+        for (int i = 0; i < 20; i++) {
+            boolean pudoMover = tablero.moverAbajo();
+            if (!pudoMover) {
+                colisiono = true;
+                break;
+            }
+        }
+
+        assertTrue(colisiono, "La segunda pieza debe colisionar con la primera y detenerse");
+    }
+
+    @Test
     public void testPiezaICaidaLibre() {
         Juego juego = new Juego(4, 6);
         Tablero tablero = juego.getTablero();
